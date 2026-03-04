2518266f04737b722cf481f140d071c66e84f58d
camilacoutodeoliveira <camilacoutodeoliveira@gmail.com>
2026-03-04 19:31:34 -0300

    Concepts of useMemo

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import "./styles/global.css";
 import { useEffect, useState } from "react";
 import { Header } from "./components/Header/Header";
-import "./styles/global.css";
 import { Tasks } from "./components/Tasks/Tasks";
 import { Refs } from "./components/Concepts/Refs";
+import { Memoization } from "./components/Concepts/Memoization";
 
 function App() {
   return (
@@ -10,6 +11,9 @@ function App() {
       <Header />
       <Tasks />
       {/* <Refs /> */}
+      <Memoization
+        financialData={{ incomes: [50, 30, 20], outcomes: [5, 8, 4] }}
+      />
     </>
   );
 }
---
+++ src/components/Concepts/Memoization.tsx
@@ -0,0 +1,36 @@
+import { useMemo, useState } from "react";
+
+interface MemoizationProps {
+  financialData: {
+    incomes: number[];
+    outcomes: number[];
+  };
+}
+export const Memoization: React.FC<MemoizationProps> = ({ financialData }) => {
+  const [showValues, setShowValues] = useState(true);
+  const totalIncomes = useMemo(() => {
+    return financialData.incomes.reduce((total, income) => {
+      return (total += income);
+    }, 0);
+  }, [financialData.incomes]);
+
+  const totalOutcomes = useMemo(() => {
+    return financialData.outcomes.reduce((total, outcome) => {
+      return (total += outcome);
+    }, 0);
+  }, [financialData.outcomes]);
+
+  return (
+    <div>
+      <h1>Memoization</h1>
+      <h2>useMemo</h2>
+
+      <p>{`Total de Receitas: R$ ${showValues ? totalIncomes : "XXX"}`}</p>
+      <p>{`Total de Despesas: R$ ${showValues ? totalOutcomes : "XXX"}`}</p>
+      <br />
+      <button onClick={() => setShowValues(!showValues)}>
+        {showValues ? "Ocultar valors" : "Mostrar valores"}
+      </button>
+    </div>
+  );
+};
